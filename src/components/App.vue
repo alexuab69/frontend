f0df6ed22b7f46393aee7536084bafe6e604fbca
alexuab69 <1634223@uab.cat>
2025-06-02 17:10:03 +0200

    index

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,57 @@
 <template>
-    
+  <BaseBackground />
+  <BasePlayer
+    :state="playerState"
+    :capturedFish="capturedFish"
+    @animationFinished="onAnimationFinished"
+    @showingCaughtFish="onShowingCaughtFish"
+  />
+  <Hud
+    :enableActionButtonTrigger="enableActionButtonTrigger"
+    :showCaughtFishTrigger="showCaughtFishTrigger"
+    @setPlayerState="onSetPlayerState"
+    @setCapturedFish="onSetCapturedFish"
+  />
 </template>
+  
+  
+<script setup>
 
-<script>
 export default {
   name: "App"
 };
+
+import { ref } from 'vue'
+
+// Componentes base
+import BaseBackground from './base_components/BaseBackground.vue'
+import BasePlayer from './base_components/BasePlayer.vue'
+
+// Componentes propios
+import Hud from './components/Hud.vue'
+
+// Estado y funciones
+const playerState = ref('standing')
+const capturedFish = ref('')
+const enableActionButtonTrigger = ref(0)
+const showCaughtFishTrigger = ref(0)
+
+// Manejadores de eventos
+function onAnimationFinished() {
+  enableActionButtonTrigger.value++
+}
+
+function onShowingCaughtFish() {
+  showCaughtFishTrigger.value++
+}
+
+function onSetPlayerState(newState) {
+  playerState.value = newState
+}
+
+function onSetCapturedFish(fishId) {
+  capturedFish.value = fishId
+}
 </script>
 
 <style>
---
+++ src/components/Hud.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref, watch } from 'vue'
+import Minigame from './Minigame.vue'
+import BaseCaptures from '../base_components/BaseCaptures.vue'
+import BaseAttempt from '../base_components/BaseAttempt.vue'
+import BaseYellowIndicator from '../base_components/BaseYellowIndicator.vue'
+import BaseActionButton from '../base_components/BaseActionButton.vue'
+import CaughtFishDialog from './CaughtFishDialog.vue'
+
+import { DIFFICULTY_TO_FISH_TYPE, PULL_ROD_TIMEOUT_MS } from '../globals.js'
+
+// Props
+defineProps({
+  showCaughtFishTrigger: Number,
+  enableActionButtonTrigger: Number
+})
+
+// Emits
+const emit = defineEmits(['setPlayerState', 'setCapturedFish'])
+
+// Estado interno
+const attempts = ref([])
+const minigameVisible = ref(false)
+const minigameDifficulty = ref('')
+const actionButtonText = ref('CAST')
+const actionButtonDisabled = ref(false)
+const yellowIndicatorTrigger = ref(0)
+const showCaughtFishDialog = ref(false)
+
+// Watchers de triggers
+watch(() => showCaughtFishTrigger, () => {
+  showCaughtFishDialog.value = true
+})
+
+watch(() => enableActionButtonTrigger, () => {
+  actionButtonDisabled.value = false
+})
+
+</script>
